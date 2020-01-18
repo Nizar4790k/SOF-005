0cf2cf3e00e7ca9cc202f48d8eb10b1e1a9a58bb
Nizar <nizar4790k@gmail.com>
2020-01-18 16:50:36 -0400

    tarea3 finished

---
+++ Tarea3/Controllers/PersonalController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea3.Controllers
+{
+    public class PersonalController : Controller
+    {
+       
+        public ActionResult Subject()
+        {
+            return View();
+        }
+
+        public ActionResult Movie()
+        {
+            return View();
+        }
+
+        public ActionResult Hobbie()
+        {
+            return View();
+        }
+
+    }
+}
