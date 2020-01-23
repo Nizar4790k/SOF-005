128c5f5a9aa9c03fe01e82d17d08391caee72e83
Nizar <nizar4790k@gmail.com>
2020-01-23 18:03:39 -0400

    lab1 created

---
+++ lab1/Controllers/FormController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab1.Models;
+
+
+namespace lab1.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Result(Person person)
+        {
+            return View(person);
+        }
+    }
+}
---
+++ lab1/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab1.Models
+{
+    public class Person
+    {
+        private string name;
+      
+
+        public string Name { get; set; }
+        public string Apellido { get; set; }
+        public int Age { get; set; }
+        public string Dni { get; set; }
+        public string Phone { get ; set ; }
+        public string Email { get ; set; }
+
+
+
+
+    }
+}
