d556d53821cd3966bad85ba38be4e00a67004945
Nizar <nizar4790k@gmail.com>
2020-02-06 18:17:13 -0400

    helper finished

---
+++ foroHtmlHelpers/HtmlHelpers/Controllers/FormController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelpers.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            ViewBag.nombre = Request.Form["name"];
+            ViewBag.edad = Request.Form["edad"];
+            ViewBag.cedula = Request.Form["id"];
+            String destino = Request.Form["destino"];
+            ViewBag.destino = null;
+
+            switch (destino)
+            {
+                case "PLAYA":
+
+                    ViewBag.destino = "https://media.blastness.info/276/top/Playa-Rincon1-top.jpg";
+
+                    break;
+
+                case "MONTANA":
+
+                    ViewBag.destino = "https://ciupico.files.wordpress.com/2016/02/montain-song.jpg";
+
+                    break;
+
+                case "USA":
+                    ViewBag.destino = "https://www.rounds.senate.gov/imo/media/image/flagrequestbaner.jpg";
+                    break;
+
+                default:
+                    ViewBag.destino = "https://www.comunidadbaratz.com/wp-content/uploads/Libros-infantiles-y-escritores-representan-a-Europa.jpg";
+                    break;
+
+                   
+            }
+           
+
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ foroHtmlHelpers/HtmlHelpers/Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HtmlHelpers.Models
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+
+        public int Edad { get; set; }
+
+        public Destino destino{get; set;}
+
+
+
+
+
+    }
+
+    public enum Destino
+    {
+        PLAYA,MONTANA,USA,EUROPA
+    }
+
+
+}
